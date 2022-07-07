0fe9253b253709b6d1fe86eff9251452ef374140
turfguy <ksm0811@ajou.ac.kr>
2022-07-07 19:11:40 +0900

    like,comment

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,20 +1,32 @@
-import { EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined } from "@ant-design/icons";
+import { EllipsisOutlined,HeartTwoTone, HeartOutlined, MessageOutlined, RetweetOutlined } from "@ant-design/icons";
 import { Button, Card, Popover } from "antd";
 import {ButtonGroup,  } from "antd/lib/button/button-group";
 import { useSelector } from "react-redux";
 import PropTypes from 'prop-types';
+import PostImages from "./PostImages";
+import { useState } from "react";
+import React from "react";
+
 
 const PostCard = ({post})=>
 {
     const id = useSelector((state)=> state.user.me?.id);
+    const [liked,setLiked] = useState(false);
+    const [commentFormOpened, setCommmentFormOpened] =  useState(false);
+
+    function onToggleLike ()
+    {
+        liked? setLiked(false): setLiked(true)
+    };
 
     return(
-        <div>
+        <div style={{marginBottom : 20 }}>
             <Card
             cover={post.Images[0] && <PostImages images={post.Images}/>}
             actions={[
                 <RetweetOutlined key="retweet"/>,
-                <HeartOutlined key="heart"/>,
+                liked? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} /> : 
+                <HeartOutlined key="heart" onClick={onToggleLike}/> ,
                 <MessageOutlined key="comment"/>,
                 <Popover key="more" content={(
                     <Button.Group>
---
+++ front/components/PostImages.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const PostImages = ({images})=>
+{
+    return(
+        <>
+            제작중
+        </>
+    )
+};
+
+PostImages.propTypes = {
+    images : PropTypes.arrayOf(PropTypes.object)
+}   
+export default PostImages;
