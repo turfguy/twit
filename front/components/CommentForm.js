3f4bee9e770bf0a7398c34636bf9bc107eef8c69
turfguy <ksm0811@ajou.ac.kr>
2022-07-19 13:37:49 +0900

    commentFunction

--- front/components/CommentForm.js
+++ front/components/CommentForm.js
@@ -1,12 +1,31 @@
-import {Form} from 'antd';
+import {Form,Button,Input} from 'antd';
+import React, { useCallback, useState } from 'react';
+import useInput from '../hooks/useInput';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 const CommentForm = ({post})=>
 {
+    const [commentText, onChangeCommentText] = useInput('');
+    const id = useSelector((state)=> state.user.me?.id)
+    const onSubmitForm  = useCallback(()=>
+    {
+        console.log(post.id,commentText);
+    }, [commentText]);
+
     return(
-        <Form>
-            폼
+        <Form onFinish={onSubmitForm}>
+            <Form.Item style={{ position:"relative" , margin: 0}}>
+                <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4}/>
+                <Button type='primary' htmlType='submit' style={{position: 'absolute', right: 0, bottom: -40}}>작성</Button>
+            </Form.Item>
         </Form>
     )
+}
+
+CommentForm.propTypes = 
+{
+    post: PropTypes.object.isRequired
 }
 
 export default CommentForm;
